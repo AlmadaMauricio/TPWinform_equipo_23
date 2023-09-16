79816c3c6b3ab05f8d4f15b743e5f20fd54ac4a6
mauriciogaston.almada <mauriciogaston.almada@gmail.com>
2023-09-16 11:39:55 -0300

    clase marcasNegocio

---
+++ Negocio/MarcasNegocio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class MarcasNegocio
+    {
+        public List<Marcas> listar()
+        {
+            List<Marcas> lista = new List<Marcas>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("Select Id, Descripcion From Marcas");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Marcas aux = new Marcas();
+                    aux.IdMarca = (int)datos.Lector["Id"];
+                    aux.DescripcionMarca = (string)datos.Lector["Descripcion"];
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
