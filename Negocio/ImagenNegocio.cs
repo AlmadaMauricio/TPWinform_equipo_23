48254f6fa3815bfad0460af2d5095be35b459530
mauriciogaston.almada <mauriciogaston.almada@gmail.com>
2023-09-16 11:48:18 -0300

    ImagenNegocio

---
+++ Negocio/ImagenNegocio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class ImagenNegocio
+    {
+        public List<Imagenes> listar()
+        {
+            List<Imagenes> lista = new List<Imagenes>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("Select Id, IdArticulo, ImagenUrl From Imagenes");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Imagenes aux = new Imagenes();
+                    aux.IdImagen = (int)datos.Lector["Id"];
+                    aux.IdArticulo = (int)datos.Lector["IdArticulo"];
+                    aux.ImagenUrl = (string)datos.Lector["ImagenUrl"]; 
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
